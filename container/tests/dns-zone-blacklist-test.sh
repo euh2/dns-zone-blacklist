76c9d54ae2fccaea7cc6056e47a555693e0ce059
euh2 <tb@tobeon.net>
2021-01-27 20:02:15 +0100

    DNS syntax tests working for all variants and formats

--- container/tests/dns-zone-blacklist-test.sh
+++ container/tests/dns-zone-blacklist-test.sh
@@ -3,21 +3,21 @@
 url="https://api.github.com/repos/euh2/dns-blacklists/contents/pub"
 folders=$(curl -s --insecure -H "Accept: application/vnd.github.v3+json" $url \
 | jq '.[] | select( .type == "dir" ) | .name' | tr -d \")
-# unblound_nxdomain=`
-# server:
-# 	include: /pub/base/unbound/unbound-nxdomain.blacklist`
-# echo $folders
 for i in $folders
 do
     echo "------------ $i ------------"
     echo Bind
+    echo "______"
     named-checkconf /pub/$i/bind/zones.blacklist || exit 1
     named-checkzone dns-zone-blacklist /pub/$i/bind/bind-nxdomain.blacklist || exit 1
     echo Dnsmasq
+    echo "______"
     dnsmasq --test --conf-file="/pub/$i/dnsmasq/dnsmasq.blacklist" || exit 1
     dnsmasq --test --conf-file="/pub/$i/dnsmasq/dnsmasq-server.blacklist" || exit 1
 done
 
+echo "---- Unbound (all enabled variants) ----"
 echo Unbound
-# unbound-checkconf /pub/$i/tests/unbound.conf || exit 1
+echo "______"
+unbound-checkconf /tests/unbound.conf 2>/dev/null || exit 1
 unbound-checkconf /tests/unbound-nxdomain.conf 2>/dev/null || exit 1
